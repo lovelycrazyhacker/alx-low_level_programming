91a345f980e3698e381be869db9a7ed27941e2ad
lovelycrazyhacker <asherpride112@gmail.com>
2022-09-08 14:51:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-
-
 /**
  *
- * * main - print the string in the put function
+ * main - print the string in the put function
  *
- * *
  *
- * * Description: using the main function
  *
- * * this program prints "Programming is like building a multilingual puzzle
+ * Description: using the main function
  *
- * * Return: 0
+ * this program prints "Programming is like building a multilingual puzzle
  *
- * */
+ * Return: 0
+ *
+ */
 
 int main(void)
 {
