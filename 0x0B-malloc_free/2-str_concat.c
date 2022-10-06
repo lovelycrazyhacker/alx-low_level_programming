678b92756a5e61e968030950ae0c348a85da7c99
lovelycrazyhacker <asherpride112@gmail.com>
2022-10-06 09:30:59 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; i++)
-			size++
+			size++;
+	if (s2 != NULL)
+		for (i = 0; s2[i] != '\0'; i++)
+			size++;
 	str = malloc(sizeof(char) * (size + 1));
 	if (str == NULL)
 		return (NULL);
